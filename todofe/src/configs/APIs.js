b152360dceda9d90e42cf952ebfe16662e6ec084
Lê Thanh Danh <118066468+ThanhhDanh@users.noreply.github.com>
2025-02-25 14:16:01 +0700

    Update APIs.js

--- todofe/src/configs/APIs.js
+++ todofe/src/configs/APIs.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-const BASE_URL = 'https://todo-wxki.onrender.com/';
-=======
 const BASE_URL = 'https://todo-9nq4.onrender.com/';
->>>>>>> c8675b2 (fixed ID)
 
 export const endpoints = {
     tasks: '/tasks',
